810418fa6f53627385ff4c4207af02776c99631b
sandrabosk <sandraboskovic@hotmail.com>
2020-10-26 17:49:11 +0100

    update error handler, rearrange middleware order in app js

--- app.js
+++ app.js
@@ -5,10 +5,20 @@ const express = require('express');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const path = require('path');
+const createError = require('http-errors');
+
 // require all the packages you install
 // ... here
 
 const app = express();
+
+// Middleware Setup
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // require database configuration
 require('./configs/db.config');
@@ -16,22 +26,24 @@ require('./configs/db.config');
 // require CORS (Cross-Origin Resource Sharing)
 // ... here
 
-// Middleware Setup
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
 // require session
 // ... here
 
 // require passport
 // ... here
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
 // routes middleware
 app.use('/', require('./routes/index.routes'));
+
+// Catch missing routes and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// Catch all error handler
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ type: 'error', error: { message: error.message } });
+});
 
 module.exports = app;
